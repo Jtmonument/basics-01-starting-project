9b87ac539f735924d01d5ac18cd53774316a77d7
Jonathan Taylor <jontaylor091@gmail.com>
2023-09-02 11:19:38 -0500

    2.27.0.1 Functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,12 @@
 let currentResult = 5;
 
+function add(a, b) {
+    const result = `Result is ${a + b}`;
+    alert(result);
+}
+
+add(2, 2);
+
 let output = `${currentResult}`;
 
 outputResult(output, '');
