c59415875c8ea3ee5fa3e06f134b71985fcf945f
Jonathan Taylor <jontaylor091@gmail.com>
2023-09-02 12:07:40 -0500

    2.39.0.1 Connecting Buttons to Functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,8 +1,35 @@
 let currentResult = 0;
+let calculationDescription;
 
 function add() {
-    currentResult += +userInput.value;
-    outputResult(currentResult, '');
+    const input = userInput.value;
+    calculationDescription = `${currentResult} + ${input}`;
+    currentResult += +input;
+    outputResult(currentResult, calculationDescription);
+}
+
+function subtract() {
+    const input = userInput.value;
+    calculationDescription = `${currentResult} - ${input}`;
+    currentResult -= +input;
+    outputResult(currentResult, calculationDescription);
+}
+
+function multiply() {
+    const input = userInput.value;
+    calculationDescription = `${currentResult} x ${input}`;
+    currentResult *= +input;
+    outputResult(currentResult, calculationDescription);
+}
+
+function divide() {
+    const input = userInput.value;
+    calculationDescription = `${currentResult} / ${input}`;
+    currentResult /= +input;
+    outputResult(currentResult, calculationDescription);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
